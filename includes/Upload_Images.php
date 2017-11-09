30ce1a25bd838eb49a9cc058ac5de778207c5d96
BeardedGinger <josh@limecuda.com>
2017-11-09 12:47:20 -0500

    include responsive breakpoints

--- includes/Upload_Images.php
+++ includes/Upload_Images.php
@@ -74,6 +74,15 @@ class Upload_Images {
 				'use_filename' => true,
 				'background'   => '#383838',
 				'opacity'      => 25,
+				'responsive_breakpoints' => [
+					[
+						'create_derived' => false,
+						'bytes_step'		 => 20000,
+						'min_width'			=> 200,
+						'max_width'			=> 1000,
+						'max_images'		 => 20,
+					],
+				],
 			];
 
 			$response = \Cloudinary\Uploader::upload( $file, $api_args );
